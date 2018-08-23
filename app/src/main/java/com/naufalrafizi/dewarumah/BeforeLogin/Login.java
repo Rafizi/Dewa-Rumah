7cb92c314457614329e610d630c8adb98872f79d
nr <naufalclas23@gmail.com>
2018-08-23 09:21:58 +0700

    first commit to git

---
+++ app/src/main/java/com/naufalrafizi/dewarumah/BeforeLogin/Login.java
@@ -0,0 +1,15 @@
+package com.naufalrafizi.dewarumah.BeforeLogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naufalrafizi.dewarumah.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/naufalrafizi/dewarumah/BeforeLogin/SignUp.java
@@ -0,0 +1,15 @@
+package com.naufalrafizi.dewarumah.BeforeLogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naufalrafizi.dewarumah.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
